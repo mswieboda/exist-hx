d91f2f0439ea19be785aa2c739f5d0f13c1096db
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-04-02 17:27:50 -0500

    adds tile grid

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,29 +6,10 @@ class PlayState extends FlxState {
   var player: Player;
 
   override public function create() {
+    var grid = new TileGrid(0, 0, 15, 30, AssetPaths.tile__png, 0xFF009900, 0xFF007700);
     player = new Player(0xFF00FF00);
 
-    var tiles = [
-      new Tile(0, 0),
-      new Tile(32, 0),
-      new Tile(64, 0),
-
-      new Tile(16, 8, 0xFF00FF00),
-      new Tile(16 + 32, 8, 0xFF00FF00),
-      new Tile(16 + 64, 8, 0xFF00FF00),
-
-      new Tile(0, 16),
-      new Tile(32, 16),
-      new Tile(64, 16),
-      new Tile(0, 32),
-      new Tile(32, 32),
-      new Tile(64, 32)
-    ];
-
-    for (tile in tiles) {
-      add(tile);
-    }
-
+    add(grid.tiles);
     add(player.units);
 
     super.create();
--- source/Tile.hx
+++ source/Tile.hx
@@ -5,16 +5,20 @@ import flixel.group.FlxGroup;
 import flixel.util.FlxColor;
 
 class Tile extends FlxSprite {
+  public static inline var GRID_WIDTH: Int = 32;
+  public static inline var GRID_HEIGHT: Int = 16;
+
   public function new(
     x: Float,
     y: Float,
+    ?asset: String = AssetPaths.tile__png,
     ?color: FlxColor = 0xFFFFFFFF
   ) {
     super(x, y);
 
     this.color = color;
 
-    loadGraphic(AssetPaths.tile__png, false, 32, 16);
+    loadGraphic(asset, false, GRID_WIDTH, GRID_HEIGHT);
   }
 
   override function update(elapsed: Float) {
