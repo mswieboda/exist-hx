86ba19275ba8ddb0af305f1e583f8585eed49cb4
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-04-02 16:44:24 -0500

    adds iso player sprite, adds initial tile

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -8,6 +8,27 @@ class PlayState extends FlxState {
   override public function create() {
     player = new Player(0xFF00FF00);
 
+    var tiles = [
+      new Tile(0, 0),
+      new Tile(32, 0),
+      new Tile(64, 0),
+
+      new Tile(16, 8, 0xFF00FF00),
+      new Tile(16 + 32, 8, 0xFF00FF00),
+      new Tile(16 + 64, 8, 0xFF00FF00),
+
+      new Tile(0, 16),
+      new Tile(32, 16),
+      new Tile(64, 16),
+      new Tile(0, 32),
+      new Tile(32, 32),
+      new Tile(64, 32)
+    ];
+
+    for (tile in tiles) {
+      add(tile);
+    }
+
     add(player.units);
 
     super.create();
--- source/Tile.hx
+++ source/Tile.hx
@@ -4,17 +4,17 @@ import flixel.FlxSprite;
 import flixel.group.FlxGroup;
 import flixel.util.FlxColor;
 
-class Unit extends FlxSprite {
+class Tile extends FlxSprite {
   public function new(
     x: Float,
     y: Float,
-    color: FlxColor
+    ?color: FlxColor = 0xFFFFFFFF
   ) {
     super(x, y);
 
     this.color = color;
 
-    loadGraphic(AssetPaths.unit__png, false, 32, 32);
+    loadGraphic(AssetPaths.tile__png, false, 32, 16);
   }
 
   override function update(elapsed: Float) {
